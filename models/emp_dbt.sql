6c174aa9b0123f23caf77a7256d9376336d1e7ff
rahulnathjogi1997@gmail.com <rahulnathjogi1997@gmail.com>
2023-03-22 05:22:44 +0000

    pre-hook, post-hook, on-run-start

--- models/emp_dbt.sql
+++ models/emp_dbt.sql
@@ -1,8 +1,15 @@
+{{ config(
+    pre_hook='create table skilled-display-380813.dbt_demo_bq.audit_started as select * from skilled-display-380813.dbt_demo_bq.audit_demo where _status="started"',
+    post_hook='create table skilled-display-380813.dbt_demo_bq.audit_ended as select * from skilled-display-380813.dbt_demo_bq.audit_demo where _status="ended"',
+    materialized='view',
+    database='skilled-display-380813'
+) }}
+
 with emp_dbt as (
-
-SELECT * FROM `skilled-display-380813.dbt_demo_bq.emp`
-
+SELECT * FROM skilled-display-380813.dbt_demo_bq.emp
 )
 
 select *
 from emp_dbt
+
+
