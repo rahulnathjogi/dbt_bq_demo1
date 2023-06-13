40ad7a240d8c58ffdc1a797a380d3c3e46bb93d1
rahulnathjogi1997@gmail.com <rahulnathjogi1997@gmail.com>
2023-06-13 09:52:01 +0000

    .

--- models/emp_dbt.sql
+++ models/emp_dbt.sql
@@ -1,10 +1,3 @@
--- {{ config(
---     pre_hook='create table skilled-display-380813.dbt_demo_bq.audit_started as select * from skilled-display-380813.dbt_demo_bq.audit_demo where _status="started"',
---     post_hook='create table skilled-display-380813.dbt_demo_bq.audit_ended as select * from skilled-display-380813.dbt_demo_bq.audit_demo where _status="ended"',
---     materialized='view',
---     database='skilled-display-380813'
--- ) }}
-
 with emp_dbt as (
 SELECT * FROM skilled-display-380813.dbt_demo_bq.emp
 )
