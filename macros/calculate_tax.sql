2099903dde24ceff7d446800462b8f7ef93f7e54
rahulnathjogi <108333717+rahulnathjogi@users.noreply.github.com>
2024-03-03 19:23:23 +0530

    Update calculate_tax.sql

--- macros/calculate_tax.sql
+++ macros/calculate_tax.sql
@@ -1,6 +1,6 @@
 {% macro calculate_tax(salary) %}
   case
-    when {{ salary }} <= 10000 then {{ salary }} * 0.05
+    when {{ salary }} <= 10000 then {{ salary }} * 0.05555555
     when {{ salary }} > 10000 and {{ salary }} <= 20000 then {{ salary }} * 0.1
     else {{ salary }} * 0.3
   end
